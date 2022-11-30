de49807e90387d2b354231052c7ef2a62e052546
BaiSugar <97774724+BaiSugar@users.noreply.github.com>
2022-11-30 17:50:54 +0800

    Update MapMarksManager.cs

--- src/GameServer/Systems/Player/MapMarksManager.cs
+++ src/GameServer/Systems/Player/MapMarksManager.cs
@@ -47,8 +47,7 @@ namespace Weedwacker.GameServer.Systems.Player
                 y = 350;
             }
             Vector3 pos = new Vector3(markMap.Mark.Pos.X,y, markMap.Mark.Pos.Z);
-            await player.World.TransferPlayerToSceneAsync(player, Enums.EnterReason.TransPoint, markMap.Mark.SceneId == (uint)player.SceneId ? EnterType.Goto : EnterType.Jump, (int)markMap.Mark.SceneId, pos, true);
-            await player.SendPacketAsync(new PacketSceneEntityAppearNotify(player.TeamManager.GetCurrentAvatarEntity(), pos, VisionType.Born,0));
+            await player.World.TransferPlayerToSceneAsync(player, Enums.EnterReason.TransPoint, markMap.Mark.SceneId == (uint)player.SceneId ? EnterType.Goto : EnterType.Jump, (int)markMap.Mark.SceneId, pos, false);
         }
     }
 }
