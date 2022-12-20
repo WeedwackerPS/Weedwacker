1c91f6299aeb7e1408d6fdae3f85ac7c6f6d99e1
oian-cheng <jijinchengcn@gmail.com>
2022-12-20 11:23:02 +0800

    fix text error

--- src/GameServer/Systems/Player/ProgressManager.cs
+++ src/GameServer/Systems/Player/ProgressManager.cs
@@ -44,8 +44,8 @@ namespace Weedwacker.GameServer.Systems.Player
             var filter = Builders<ProgressManager>.Filter.Where(p => p.OwnerId == Owner.GameUid);
             var update = Builders<ProgressManager>.Update.Set(w => w.UnlockedScenePoints, UnlockedScenePoints);
             await DatabaseManager.UpdateProgressAsync(filter, update);
-            //await Owner.Invenrtory.AddItemByIdAynsc(201, 5, ActionReason.UnlockPointReward);
-            // await Owner.Inventory.AddItemByIdAsync(102, isStatue ? 50 : 10, ActionReason.UnlockPointReward);
+            //await Owner.Inventory.AddItemByIdAynsc(201, 5, ActionReason.UnlockPointReward);
+            //await Owner.Inventory.AddItemByIdAsync(102, isStatue ? 50 : 10, ActionReason.UnlockPointReward);
             await Owner.SendPacketAsync(new PacketScenePointUnlockNotify((uint)sceneId, (uint)pointId));
             return true;
         }
