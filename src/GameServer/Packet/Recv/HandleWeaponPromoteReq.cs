9e0bd57e3e1dd5f0ff717558a8c38660204e3aad
24karatt <thekillerdarky@gmail.com>
2022-12-23 13:24:01 +0100

    handle weaponpromote packets

---
+++ src/GameServer/Packet/Recv/HandleWeaponPromoteReq.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weedwacker.GameServer.Enums;
+using Weedwacker.Shared.Network.Proto;
+
+namespace Weedwacker.GameServer.Packet.Recv
+{
+
+    [OpCode((ushort)OpCode.WeaponPromoteReq)]
+    internal class HandleWeaponPromoteReq : BaseHandler
+    {
+        public override async Task HandleAsync(Connection session, byte[] header, byte[] payload)
+        {
+            WeaponPromoteReq req = WeaponPromoteReq.Parser.ParseFrom(payload);
+            await session.Player.Inventory.promoteWeaponAsync(req.TargetWeaponGuid);
+        }
+    }
+}
+
