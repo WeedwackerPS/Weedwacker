65154abcf3ed01637f16b3124e17b6ec1a1d6534
tamilpp25 <tamilpp25@gmail.com>
2022-12-25 21:34:22 +0530

    ConfigAbilities 3.3

---
+++ src/GameServer/Data/BinOut/Ability/Temp/AbilityMixins/CountCheckEventMixin.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Weedwacker.GameServer.Data.BinOut.Ability.Temp.Actions;
+
+namespace Weedwacker.GameServer.Data.BinOut.Ability.Temp.AbilityMixins
+{
+    internal class CountCheckEventMixin : BaseAbilityMixin
+    {
+        [JsonProperty] public readonly string EJKFMLLBPJM;
+        [JsonProperty] public readonly float EBHLJAPLEDL;
+        [JsonProperty] public readonly int checkCount;
+        [JsonProperty] public readonly ConfigAbilityAction[] actionQueue;
+    }
+}
---
+++ src/GameServer/Data/BinOut/Ability/Temp/AbilityMixins/NahidaHollowFlowerMixin.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Weedwacker.GameServer.Data.BinOut.Ability.Temp.AbilityMixins
+{
+    internal class NahidaHollowFlowerMixin : BaseAbilityMixin
+    {
+        [JsonProperty] public readonly string effectPattern;
+        [JsonProperty] public readonly string effectTrigger;
+        [JsonProperty] public readonly string interval;
+        [JsonProperty] public readonly string maxCount;
+        [JsonProperty] public readonly string createMaxNumPerInterval;
+        [JsonProperty] public readonly string createOffset;
+        [JsonProperty] public readonly string maxRange;
+        [JsonProperty] public readonly string destroyRange;
+    }
+}
